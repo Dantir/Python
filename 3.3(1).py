890f1bb3e1bf6109a89e64f7739007796efa2113
Dantir <45596475+Dantir@users.noreply.github.com>
2019-06-25 17:22:03 +0200

    Update 3.3(1).py

--- 3.3(1).py
+++ 3.3(1).py
@@ -4,6 +4,4 @@ def function(num1,num2):
     return num
 def main():
     print(function(input("Please write the first number: "),input("Please write the second number: ")))
-    #num2=int(input("Please write the second number: "))
-    #print(function(input("The geometric mean of it is : "))
 main()
