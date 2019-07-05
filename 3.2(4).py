3073fc727a6d177cae0c73dba6610d134a59b23a
Dantir <45596475+Dantir@users.noreply.github.com>
2019-07-05 11:19:56 +0200

    Update 3.2(4).py

--- 3.2(4).py
+++ 3.2(4).py
@@ -1,4 +1,5 @@
 ##Converts Fahrenheit to Celsius and vice versa until the user writes 0
+
 i=1
 while i!=0:
     i = int(input("\nWhat conversion will you do?\nCelsius to Fahrenheit-1\nFahrenheit to Celsius-2\nif you want to exit press 0\n"))
