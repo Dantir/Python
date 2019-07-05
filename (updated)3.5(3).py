b544ecd6aa9a688379820a119229ec20456f073b
Dantir <45596475+Dantir@users.noreply.github.com>
2019-07-05 11:35:52 +0200

    Update (updated)3.5(3).py

--- (updated)3.5(3).py
+++ (updated)3.5(3).py
@@ -1,3 +1,5 @@
+##a function which accepts a number of keywords from the dictionary and prints a unique word for each number
+
 text = {0 :['Bald','Hairy'],1:['Rat','Cat','Dog'],2:['Ran','Crawled'],3:['Hastily','Slowly','Quietly','Loudly'],4:['In the sewers','On the roof']}
 def function(*numbers):
     i = 0
